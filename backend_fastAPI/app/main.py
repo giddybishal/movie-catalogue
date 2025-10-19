2b99dda0d7130a3e0ce24340cac11af70b748732
Bishal <nbishal7@gmail.com>
2025-10-19 21:09:16 +0545

    adding Dockerfile to the backend too, trying to deploy it in render

--- backend_fastAPI/app/main.py
+++ backend_fastAPI/app/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 origins = [
     "http://localhost:5173",
-    "https://movie-catalogue-frontend-jq4e.onrender.com"       
+    "https://movie-catalogue-ewla.onrender.com"       
 ]
 
 app.add_middleware(
