5c9fa27accda8d91de5f1826b0253cef4aa69691
Bishal <nbishal7@gmail.com>
2025-10-20 02:37:18 +0545

    finding ways to integrate chatbot

--- backend_fastAPI/app/services/chat_with_bot.py
+++ backend_fastAPI/app/services/chat_with_bot.py
@@ -1,6 +1,27 @@
-import ollama
+import os
+import requests
+from dotenv import load_dotenv
 
 def ollama_chatbot(message_history):
-    MODEL = 'deepseek-v3.1:671b-cloud'
-    response = ollama.chat(model=MODEL, messages=message_history)
-    return response['message']['content']
+    load_dotenv()
+    API_URL = "https://router.huggingface.co/v1/chat/completions"
+    headers = {
+        "Authorization": f"Bearer {os.environ['HF_API_TOKEN']}",
+    }
+
+    # Convert Pydantic models to dictionaries
+    serializable_message_history = [
+        {"role": msg.role, "content": msg.content} 
+        for msg in message_history
+    ]
+
+    def query(payload):
+        response = requests.post(API_URL, headers=headers, json=payload)
+        return response.json()
+
+    response = query({
+        "messages": serializable_message_history,
+        "model": "deepseek-ai/DeepSeek-V3.1:novita"
+    })
+
+    return response["choices"][0]["message"]['content']
