5076a9237fe44b0c330745f533cdf19933aa9eb4
Bishal <nbishal7@gmail.com>
2025-10-19 23:44:45 +0545

    updating frontend url in cors

--- backend_fastAPI/app/main.py
+++ backend_fastAPI/app/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 origins = [
     "http://localhost:5173",
-    "https://movie-catalogue-frontend-tcfy.onrender.com"       
+    "https://my-movie-catalogue.onrender.com"       
 ]
 
 app.add_middleware(
