d53d543d241a8d4646b28595635f05d7b1d501d4
Bishal <nbishal7@gmail.com>
2025-10-19 20:02:30 +0545

    added a Dockerfile, trying to deploy to render, once again, but through docker image this time

--- frontend_react/src/contexts/AI_ChatbotContext.jsx
+++ frontend_react/src/contexts/AI_ChatbotContext.jsx
@@ -4,6 +4,8 @@ import axios from 'axios'
 export const AI_ChatbotContext = createContext()
 
 export function AI_ChatbotProvider({children}){
+    const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
+
     const system_prompt = " You're an assitant chatbot and movie fiend, afficionado and lover Neesha, for a movie catalgoue website that allows you to browse throught the latest movies as well as search through other older movies. It also allows you the capabiliy to favourite and set to watch later the movies, if you're logged in. You should encourage the customer to login to the site if they're not. For now your job is to chat with the customers about anything movie-related. Answer anything you know and honestly tell them if you dont know something. NEVER MAKE SOMETHING UP. The answer should be on the shorter side. Generally no more than a couple of sentences. Prohibit talking about irrelevant topics and veer the customers towards the topic of website or any movie that the customer might like. Answer in plain text."
 
     const [ chatHistory, setChatHistory] = useState([{'role':'system', 'content': system_prompt}])
@@ -12,7 +14,7 @@ export function AI_ChatbotProvider({children}){
         const newHistory = [...chatHistory, { role: 'user', content: userMessage }]
         setChatHistory(newHistory)
         try{
-            const response = await axios.post('http://127.0.0.1:8000/chat_with_bot', { 'message_history' : newHistory}) 
+            const response = await axios.post(`${BACKEND_URL}/chat_with_bot`, { 'message_history' : newHistory}) 
             setChatHistory([...newHistory, {'role':'assistant', 'content': response.data} ])
             console.log(response.data)
         } catch(e){
